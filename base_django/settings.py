54bce21ce1cb8d8471c58a6f4e01f86302e5c5c8
Passing01 <ouerahim456@gmail.com>
2025-02-01 14:37:37 +0000

    installation de whitenoise

--- base_django/settings.py
+++ base_django/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoise.Middleware',
 ]
 
 ROOT_URLCONF = 'base_django.urls'
